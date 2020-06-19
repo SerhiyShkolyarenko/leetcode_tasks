5858914f4bc3a4ebded5f4e74defd863147df9b4
Serhiy Shkolyarenko <serhiy.shkolyarenko@gmail.com>
2020-06-20 02:08:38 +0300

    Updated solution for IP address validation

--- validate_ip_address/Solution.php
+++ validate_ip_address/Solution.php
@@ -1,42 +1,50 @@
 <?php
 
-class Solution {
-
+class Solution
+{
     /**
      * @param String $IP
      * @return String
      */
-    function validIPAddress($IP) {
-        if (strpos($IP, '.') !== false) {
-            //validate as IPv4
-            $parts = explode('.', $IP);
-            if (count($parts) !== 4) {
-                return 'Neither';
-            }
-            foreach ($parts as $number) {
-                if (!$this->validate255($number)) {
-                    return 'Neither';
-                }
-            }
+    public function validIPAddress($IP) {
+        if ($this->isValidV4($IP)) {
             return 'IPv4';
-        } elseif(strpos($IP, ':') !== false) {
-            //validate IPv6
-            $parts = explode(':', $IP);
-            if (count($parts) !== 8) {
-                return 'Neither';
-            }
-            foreach ($parts as $number) {
-                $number = strtolower($number);
-                if (!preg_match('/^[0-9a-f]{1,4}$/', $number)) {
-                    return 'Neither';
-                }
-            }
+        } elseif ($this->isValidV6($IP)) {
             return 'IPv6';
         }
         return 'Neither';
     }
     
-    function validate255($value)
+    private function isValidV6($IP)
+    {
+        $parts = explode(':', $IP);
+        if (count($parts) !== 8) {
+            return false;
+        }
+        foreach ($parts as $number) {
+            $number = strtolower($number);
+            if (!preg_match('/^[0-9a-f]{1,4}$/', $number)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private function isValidV4($IP)
+    {
+        $parts = explode('.', $IP);
+        if (count($parts) !== 4) {
+            return false;
+        }
+        foreach ($parts as $number) {
+            if (!$this->validateV4Item($number)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private function validateV4Item($value)
     {
         $number = (int)$value;
         if ($number < 0 || $number > 255 || $value !== (string)$number) {
--- validate_ip_address/test.php
+++ validate_ip_address/test.php
@@ -7,12 +7,14 @@ $testData = [
  ['input' => "172.0.254.1", 'output' => 'IPv4'],
  ['input' => "172.16.254.0", 'output' => 'IPv4'],
  ['input' => "172.16.254.0.", 'output' => 'Neither'], //extra dot
+ ['input' => "172.16.254:0", 'output' => 'Neither'], // : is forbidden
  ['input' => "1e2.16.254.0", 'output' => 'Neither'], // letter in address
  ['input' => "2001:0db8:85a3:0:0:8A2E:0370:7334", 'output' => 'IPv6'],
  ['input' => "2001:0db8:85a3:0000:00:8A2E:0370:7334", 'output' => 'IPv6'],
  ['input' => "2001:0db8:85a3:0000:00:8A2E:0370:7334", 'output' => 'IPv6'],
  ['input' => "22001:0db8:85a3:0000:00:8A2E:0370:7334", 'output' => 'Neither'], // extra digit
  ['input' => "2200:0hb8:85a3:0000:00:8A2E:0370:7334", 'output' => 'Neither'], // 'h' is forbidden
+ ['input' => "2200:0hb8:85a3:0:0.8A2E:0370:7334", 'output' => 'Neither'], // . is forbidden
 ];
 
 require_once('Solution.php');
